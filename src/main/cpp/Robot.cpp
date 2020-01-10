41d4c23bbf5bf0f70f17cb6c2d5e728becb6dbda
Corey Knutson <corey.knutson789@gmail.com>
2020-01-09 22:56:51 -0600

    added a new testing file

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -4,25 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
-// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+
 #include "Robot.h"
 
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -38,6 +20,25 @@ void Robot::RobotInit() {}
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
+// this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
 void Robot::RobotPeriodic() { frc2::CommandScheduler::GetInstance().Run(); }
 
 /**
